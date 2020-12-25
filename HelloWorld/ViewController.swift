0dd9031d5a9901705b1280224fe26035b07756ea
toaster08 <y10star.hon@gmail.com>
2020-12-25 23:20:46 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 山田　天星 on 2020/12/25.
+//  Copyright © 2020 toaster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
